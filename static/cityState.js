0092132b5b827e5e16496ebd20261ecb823e01a1
unknown <exgin3538@yahoo.com>
2020-05-28 11:22:51 -0400

    put cities in alphabetical order

--- static/cityState.js
+++ static/cityState.js
@@ -6,8 +6,10 @@ stateSelect.onchange = function () {
 
   fetch(`/city/${state}`).then(function (res) {
     res.json().then(function (data) {
+      let sortedCities = data.cities.sort((a, b) => (a.city > b.city ? 1 : -1));
+
       let optionHTML = '';
-      for (let city of data.cities) {
+      for (let city of sortedCities) {
         optionHTML += `<option value="${city.state_id}">${city.city}</option>`;
       }
       citySelect.innerHTML = optionHTML;
